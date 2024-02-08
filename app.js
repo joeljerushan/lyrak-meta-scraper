6f1b31b58bebce76509790e176de96de0dd8beb7
Joel Jerushan <joeljeru1@gmail.com>
2024-02-08 20:31:46 +0530

    fixes

--- app.js
+++ app.js
@@ -14,8 +14,46 @@ app.use(express.json());
 
 async function scrapeMetadata(url) {
   try {
+    fetch(url)
+      .then((result) => result.text())
+      .then((page) => {
+        const $ = cheerio.load(page);
+        var title =
+          $('meta[property="og:title"]').attr("content") ||
+          $("title").text() ||
+          $('meta[name="title"]').attr("content");
+        var description =
+          $('meta[property="og:description"]').attr("content") ||
+          $('meta[name="description"]').attr("content");
+        var url = $('meta[property="og:url"]').attr("content");
+        var site_name = $('meta[property="og:site_name"]').attr("content");
+        var image =
+          $('meta[property="og:image"]').attr("content") ||
+          $('meta[property="og:image:url"]').attr("content");
+        var icon =
+          $('link[rel="icon"]').attr("href") ||
+          $('link[rel="shortcut icon"]').attr("href");
+        var keywords =
+          $('meta[property="og:keywords"]').attr("content") ||
+          $('meta[name="keywords"]').attr("content");
+        // res.json({ title, description, url, site_name, image, icon, keywords });
+      })
+      .catch((err) => {
+        return res.json({
+          error:
+            "An error has occured, you may have inputted an incorrect url.",
+          usage: `https://${req.hostname}/?url=https://google.com`,
+        });
+      });
+
     return {
-      test: "metadata",
+      title,
+      description,
+      url,
+      site_name,
+      image,
+      icon,
+      keywords,
     };
   } catch (error) {
     console.error("Scrape error:", error);
@@ -31,36 +69,6 @@ app.get("/scrape", async (req, res) => {
     return res.json({
       error: "An error has occured, you may have inputted an incorrect url.",
       usage: `https://${req.hostname}/?url=https://google.com`,
-    });
-  fetch(req.query.url)
-    .then((result) => result.text())
-    .then((page) => {
-      const $ = cheerio.load(page);
-      var title =
-        $('meta[property="og:title"]').attr("content") ||
-        $("title").text() ||
-        $('meta[name="title"]').attr("content");
-      var description =
-        $('meta[property="og:description"]').attr("content") ||
-        $('meta[name="description"]').attr("content");
-      var url = $('meta[property="og:url"]').attr("content");
-      var site_name = $('meta[property="og:site_name"]').attr("content");
-      var image =
-        $('meta[property="og:image"]').attr("content") ||
-        $('meta[property="og:image:url"]').attr("content");
-      var icon =
-        $('link[rel="icon"]').attr("href") ||
-        $('link[rel="shortcut icon"]').attr("href");
-      var keywords =
-        $('meta[property="og:keywords"]').attr("content") ||
-        $('meta[name="keywords"]').attr("content");
-      res.json({ title, description, url, site_name, image, icon, keywords });
-    })
-    .catch((err) => {
-      return res.json({
-        error: "An error has occured, you may have inputted an incorrect url.",
-        usage: `https://${req.hostname}/?url=https://google.com`,
-      });
     });
 
   try {
